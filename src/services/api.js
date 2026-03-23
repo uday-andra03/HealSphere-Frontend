f67679a5d9464ce5173ae11b01736f10f55bc987
UDAYCHANDRA ANDRA <udayandranimblix@gmail.com>
2026-03-23 22:33:18 +0530

    update baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:9092",
+  baseURL: "https://healthcarehub-backend.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
